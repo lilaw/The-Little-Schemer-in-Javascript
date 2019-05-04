80511089b9bb67816b371b64512b91ec2e030148
Ellis <takialog@gmail.com>
2019-05-04 14:22:59 +0800

    03

---
+++ src/03.js
@@ -0,0 +1,78 @@
+import { isNull, isEq } from "./little";
+// It takes an atom and a lat as its arguments,
+// and makes a new lat with the first occurrence
+// of the atom in the old lat removed.
+function rember(a, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return [];
+  if (isEq(a, first)) return rest;
+  return [first, ...rember(a, rest)];
+}
+
+// builds another list composed of the first S-expression of each internal list.
+function first(l) {
+  let [foremost, ...rest] = l;
+  if (isNull(foremost)) return [];
+  return [...foremost.slice(0, 1), ...first(rest)];
+}
+
+// return new list inserted to the right of the first occurrence of old
+function insertR(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return [];
+  if (isEq(old, first)) return [old, fresh, ...rest]
+  return [first, ...insertR(fresh, old, rest)]
+}
+
+function insertL(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return [];
+  if (isEq(old, first)) return [fresh, lat]
+  return [first, ...insertR(fresh, old, rest)]
+}
+
+// subst replaces the first occurrence of old in the lat with new
+function subst(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return [];
+  if (isEq(old, first)) return [fresh, ...rest]
+  return [first, ...subst(fresh, old, rest)]
+}
+
+// subst2 replaces either the first occurrence of o1 or the first occurrence of o2 by new 
+function subst2(fresh, o1, o2, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return [];
+  if (isEq(o1, first) || isEq(o2, first)) return [fresh, ...rest]
+  return [first, ...subst2(fresh, o1, o2, rest)]
+}
+
+// multirember remove  all occurrences a 
+function multirember(a, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return []
+  if (isEq(a, first)) return multirember(a, rest)
+  return [first, ...multirember(a, rest)]
+
+}
+
+function multiinsertR(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return []
+  if (isEq(old, first)) return [old, fresh, ...multiinsertR(fresh, old, rest)]
+  return [first, ...multiinsertR(fresh, old, rest)]
+}
+
+function multiinsertL(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isNull(first)) return []
+  if (isEq(old, first)) return [fresh, old, ...multiinsertL(fresh, old, rest)]
+  return [first, ...multiinsertL(fresh, old, rest)]
+}
+
+function multisubst(fresh, old, lat) {
+  let [first, ...rest] = lat;
+  if (isEq(old, first)) return [fresh, multisubst(fresh, old, rest)]
+  return [first, ...multisubst(fresh, old, rest)]
+}
+export { rember, first, insertR, insertL, subst, subst2, multirember, multiinsertR, multiinsertL, multisubst};
---
+++ src/__tests__/03.js
@@ -0,0 +1,82 @@
+import { rember, first, insertR, subst, subst2, multirember, multiinsertR } from "../03";
+
+test("rember should remove first occurrence atom in a list", () => {
+  let a = "cup";
+  let noA = "puc";
+  let list = ["coffee", "cup", "tea", "cup", "and", "hick", "cup"];
+  let removedFirstOccuAtomList = list.reduce(
+    (acc, cur, ind) => (ind === list.indexOf(a) ? acc : [...acc, cur]),
+    []
+  );
+
+  expect(rember(a, list)).toEqual(removedFirstOccuAtomList);
+  expect(rember(noA, list)).toEqual(list);
+});
+
+test("first return a array of first element", () => {
+  let l = [
+    ["apple", "peach", "pumpkin"],
+    ["plum", "pear", "cherry"],
+    ["grape", "raisin", "pea"],
+    ["bean", "carrot", "eggplant"]
+  ];
+  let listOfFirstEle = ["apple", "plum", "grape", "bean"]
+
+  expect(first(l)).toEqual(listOfFirstEle)
+});
+
+
+test('insertR return new list inserted to the right of the first occurrence of old', () => {
+  let old  = "fudge";
+  let fresh = "topping";
+  let lat = `ice cream with ${old} for dessert`.split(' ')
+  let resule = `ice cream with ${old} ${fresh} for dessert`.split(' ')
+
+  expect(insertR(fresh, old, lat)).toEqual(resule)
+})
+
+test('subst replaces the first occurrence of old in the lat with new', () => {
+  let old  = "fudge";
+  let fresh = "topping";
+  let lat = `ice cream with ${old} for dessert`.split(' ')
+  let resule = `ice cream with ${fresh} for dessert`.split(' ')
+
+  expect(subst(fresh, old, lat)).toEqual(resule)
+})
+
+test('subst2 replaces either the first occurrence of o1 or the first occurrence of o2 by new', () => {
+  let o1  = "cream";
+  let o2 = "dessert"
+  let fresh = "banana";
+  let lat = `ice cream with fudge for dessert`.split(' ')
+  let resule = `ice ${fresh} with fudge for dessert`.split(' ')
+
+  expect(subst2(fresh, o1, o2, lat)).toEqual(resule)
+})
+
+
+test("multirember remove  all occurrences a ", () => {
+  let a = "cup";
+  let list = ["coffee", "cup", "tea", "cup", "and", "hick", "cup"];
+  let resule = list.filter(el => el !== a)
+
+  expect(multirember(a, list)).toEqual(resule)
+})
+
+test('multiinsertR insert new to the right of all of old', () => {
+  let old = "cup";
+  let fresh = 'a'
+  let list = ["coffee", "cup", "tea", "cup", "hick", "mug"];
+  let resule = list.reduce((acc, cur) => cur === old? [...acc, cur, fresh] : [...acc, cur], [])
+
+  expect(multiinsertR(fresh, old, list)).toEqual(resule)
+})
+
+test('function replaces the first occurrence of old in the lat with new', () => {
+  let old  = "fudge";
+  let fresh = "topping";
+  let lat = `ice cream with ${old} for dessert`.split(' ')
+  let resule = `ice cream with ${fresh} for dessert`.split(' ')
+
+  expect(subst(fresh, old, lat)).toEqual(resule)
+})
